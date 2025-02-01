395b6aa50b70351d9de9650d516124fa513a1784
Fernando Silva <fjo.psilva@gmail.com>
2025-02-01 18:46:47 +0000

    first commit

---
+++ dags/spark_airflow.py
@@ -0,0 +1,35 @@
+import airflow
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+
+dag = DAG(
+    dag_id = "spark_airflow_testing",
+    default_args = {
+        "owner": "Fernando Silva",
+        "start_date": airflow.utils.dates.days_ago(1)
+    },
+    schedule_interval = "@daily"
+)
+
+start = PythonOperator(
+    task_id="start",
+    python_callable = lambda: print("Jobs started"),
+    dag=dag
+)
+
+scala_job = SparkSubmitOperator(
+    task_id="scala_job",
+    conn_id="spark-conn",
+    application="jobs/scala/target/scala-2.12/word-count_2.12-0.1.jar",
+    dag=dag
+)
+
+
+end = PythonOperator(
+    task_id="end",
+    python_callable = lambda: print("Jobs completed successfully"),
+    dag=dag
+)
+
+start >> [scala_job] >> end
